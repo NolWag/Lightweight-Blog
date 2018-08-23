444075f5730b4fd0d1cd7216391ad3fc2bf3bb66
NolWag <NolanDubyu@gmail.com>
2018-08-23 00:34:40 -0500

    styles

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
